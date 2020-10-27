7380c1325f82fa24bb687e24a56284ecff4f1c26
hwamil <hwamil@gmail.com>
2020-10-26 18:42:01 -1000

    deleted extra parens

--- sniper/checkout.py
+++ sniper/checkout.py
@@ -160,10 +160,9 @@ def skip_address_check(driver, customer, timeout):
             driver.find_element(By.ID, 'shippingAddressOptionRow1').click()
         except TimeoutException:
             pass
-        finally:
-            WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.ID, 'selectionButton')))
-            driver.find_element(By.ID, 'selectionButton').click()
-            logging.info('Moving onto order summary page')
+    WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.ID, 'selectionButton')))
+    driver.find_element(By.ID, 'selectionButton').click()
+    logging.info('Moving onto order summary page')
 
 
 def select_shipping_speed(driver, timeout, customer):
@@ -190,8 +189,8 @@ def select_shipping_speed(driver, timeout, customer):
         try:
             logging.info('Trying shippingOptionID2')
             WebDriverWait(driver, timeout).until(
-                EC.presence_of_element_located((By.ID, 'shippingOptionID2')))
-            driver.find_element((By.ID, 'shippingOptionID2')).click()
+                EC.element_to_be_clickable((By.ID, 'shippingOptionID2')))
+            driver.find_element(By.ID, 'shippingOptionID2').click()
         except TimeoutException:
             logging.info('Could not select shippingOptionID2')
             logging.info('Continuing with default speed')
